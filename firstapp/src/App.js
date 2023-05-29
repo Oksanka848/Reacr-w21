e2c5740cc58af4fda7d9523dd31ea60d272b63df
Oksanaka848 <okmitka@mail.ru>
2023-05-29 22:14:55 +0300

    add react-google-charts

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import { Chart } from "react-google-charts";
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Work", 11],
+  ["Eat", 2],
+  ["Commute", 2],
+  ["Watch TV", 2],
+  ["Sleep", 7],
+];
 
-function App() {
+export const options = {
+  title: "My Daily Activities",
+  is3D: true,
+};
+
+export default function App() {
   return (
-    <div className="App"></div>
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
   );
 }
-
-export default App;
